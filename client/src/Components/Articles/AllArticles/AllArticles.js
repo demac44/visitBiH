d0954f478ecaf139c7083f7abb6120bf94b5fef6
demac44 <demirumejr2@gmail.com>
2022-05-14 21:14:24 +0300

    Completely finished changing language

--- client/src/Components/Articles/AllArticles/AllArticles.js
+++ client/src/Components/Articles/AllArticles/AllArticles.js
@@ -5,7 +5,10 @@ import "./style.css"
 const AllArticles = ({articles, lang}) => {
   return (
     <div className='all-articles-container'>
+      {lang === "english" ?
       <h1 className='all-articles-title'>READ MORE ABOUT BOSNIA AND HERZEGOVINA</h1>
+      : 
+      <h1 className='all-articles-title'>ČITAJ VIŠE O BOSNI I HERCEGOVINI</h1>}
       <div className='all-articles-overlay'></div>
       <div className='all-articles'>
         {articles.map(article => <ArticleCard article={article} key={article._id} lang={lang}/>)}
